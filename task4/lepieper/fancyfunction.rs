13d49338e32ec778289ad9d6a23a5b8dc7f17277
lepieper <lepieper@uni-osnabrueck.de>
2016-07-20 11:24:27 +0200

    fix unnecessary stuff in fancyfunction.rs

--- task4/lepieper/fancyfunction.rs
+++ task4/lepieper/fancyfunction.rs
@@ -2,11 +2,7 @@ fn main() {
     println!("{:?}", sumandproduct(4, 6))
 }
 
-use std::ops::Add;
-use std::ops::Mul;
-use std::marker::Copy;
+use std::ops::{Add, Mul};
 fn sumandproduct<T: Add + Mul + Copy>(a: T, b: T) -> (<T as Add>::Output, <T as Mul>::Output) {
-    let v: (<T as Add>::Output, <T as Mul>::Output) = (a + b, a * b);
-    v
-
+    (a + b, a * b)
 }
